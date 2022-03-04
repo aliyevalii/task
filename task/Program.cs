62fcd913d5659781abdec61944d14cb67b9e7068
Ali Aliyev <tu4pw6wq5@aztu.local>
2022-03-04 14:28:31 +0400

    added task

---
+++ task/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+
+
+            //int counter = 0;
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int m = Convert.ToInt32(Console.ReadLine());
+            //for (int i = n; i < m; i++)
+            //{
+            //    if (i % 7 == 0 && i!=0)
+            //    {
+            //        counter++;
+
+            //    }
+            //}
+            //Console.WriteLine(counter);
+            #endregion
+            #region
+            //string necegunvar = Console.ReadLine();
+            //switch (necegunvar)
+            //{
+            //    case "yanvar":
+            //        Console.WriteLine("31");
+            //        break;
+            //    case "fevral":
+            //        Console.WriteLine("28 4ilden bir 29 olur");
+            //        break;
+            //    case "mart":
+            //        Console.WriteLine("31");
+            //        break;
+            //    case "aprel":
+            //        Console.WriteLine("30");
+            //        break;
+            //    case "may":
+            //        Console.WriteLine("31");
+            //        break;
+            //    case "iyun":
+            //        Console.WriteLine("30");
+            //        break;
+            //    case "iyul":
+            //        Console.WriteLine("31");
+            //        break;
+            //    case "avqust":
+            //        Console.WriteLine("31");
+            //        break;
+            //    case "sentyabr":
+            //        Console.WriteLine("30");
+            //        break;
+            //    case "oktyabr":
+            //        Console.WriteLine("31");
+            //        break;
+            //    case "noyabr":
+            //        Console.WriteLine("30");
+            //        break;
+            //    case "dekabr":
+            //        Console.WriteLine("31");
+            //        break;
+            //    default:
+            //        Console.WriteLine("bele ay yoxdur");
+            //        break;
+            }
+
+            #endregion
+        }
+    }
+}
